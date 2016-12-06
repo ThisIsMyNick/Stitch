680bbcf9463173dbadb7c5c17ba475b358c3d55c
Harry Lum <hlum1@stuy.edu>
2016-12-06 12:28:36 -0500

    add test cases

--- utils/wishlist.py
+++ utils/wishlist.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 
 def getWishlist(username):
     ans = []
@@ -8,6 +8,25 @@ def getWishlist(username):
     data = c.execute("SELECT username,games FROM wishlist WHERE username=?",(username,))
     for x in data:
         ans.append(x)
+    db.close()
     return ans
 
     
+def addWishlist(username,game):
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor()
+    data = c.execute("INSERT INTO wishlist VALUES(?,?)",(username,game,))
+    db.commit()
+    db.close()
+
+'''
+os.chdir("..")
+addWishlist("hello","a")
+addWishlist("hello","ab")
+addWishlist("hello","abc")
+addWishlist("jones","abc")
+x = getWishlist("hello")
+print(x)
+x = getWishlist("jones")
+print(x)
+'''
