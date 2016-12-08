09fc2260756957ff02a3247dc219d5870b363d2f
Harry Lum <hlum1@stuy.edu>
2016-12-08 12:35:11 -0500

    create login

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 import os
 import utils.steam_api as steam_api
 from pprint import pprint
+from utils import wishlist,login
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ def homepage():
         price = str(game['final_price'])
         price = price[:-2] + "." + price[-2:]
         games.append((name, price))
-    return render_template('homepage.html', game_data=games)
+    return render_template('homepage.html', game_data=games, regRet = regRet)
 
 if __name__ == '__main__':
     if os.path.getsize("data/database.db") == 0:
@@ -31,3 +32,29 @@ if __name__ == '__main__':
     app.debug=True
     app.secret_key = '  \x43\xd2\x34\x92\x5b\x4a\x80\xfc\xc6\xb0\x0e\x45\xdd\x51\x36\xc0\xd2\x3a\x85\x42\x57\xbb\x61\xf2\x7b\xb6\xfc\x17\x3b\x1a\xda\x5b\x6d\x7d\x0a\xff\xd3\x6f\xfa\x7c\x1b\xa8\x0f\x7f\x53\x18\x8d\x91\x16\x81'
     app.run()
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    pw = request.form["pass"]
+    un = request.form["user"]
+    tp = request.form["action"]#login vs. register
+    
+    if tp == "register":
+        regRet = auth.register(un,pw)#returns an error/success message
+        return redirect('register.html', result = regRet)
+        
+    if tp == "login":
+        text = auth.login(un,pw)#error message
+        if text == "":#if no error message, succesful go back home
+            session["Username"] = un
+            return redirect(url_for('homePage'))
+        return render_template('login.html', result = text)
+    
+    
+
+@app.route('/<username>')
+def profile():
+
+
+@app.route('/<gamepage>')
+def gamepage():
---
+++ utils/login.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+from hashlib import sha1
+import sqlite3
+
+f = "data/database.db"
+
+def login(user, password):
+    db = connect(f)
+    c = db.cursor()
+    query = ("SELECT * FROM users WHERE username=?")
+    sel = c.execute(query,(username,));
+    
+    #records with this username
+    #so should be at most one record (in theory)
+     
+    for record in sel:
+        password = sha1(password).hexdigest()
+        if (password==record[1]):
+            return "Login successful!" #no error message because it will be rerouted to mainpage
+        else:
+            return "Invalid password."#error message
+    db.close()
+    
+    return "User does not exist."#error message
+
+def register(user, password):
+    db = connect(f)
+    c = db.cursor()
+    try: #does table already exist?
+        c.execute("SELECT * FROM users")
+    except: #if not, this is the first user!
+        c.execute("CREATE TABLE users (username TEXT, password TEXT)")
+    db.commit()
+    db.close()
+    return regMain(user, password)#register helper
+
+def regMain(user, password):#register helper
+    db = connect(f)
+    c = db.cursor()
+    reg = regReqs(user, password)
+    if reg == "": #if error message is blank then theres no problem, update database
+        query = ("INSERT INTO users VALUES (?, ?)")
+        password = sha1(password).hexdigest()
+        c.execute(query, (user, password))
+        db.commit()
+        db.close()
+        return "Account created!"
+    db.commit()
+    db.close()
+    return reg#return error message
+        
+def regReqs(user, password):      #error message generator
+    if duplicate(user):          #checks if username already exists
+        return "Username already exists."
+    if " " in user:
+        return "Spaces not allowed in username."
+    return ""
+
+def duplicate(user):#checks if username already exists
+    db = connect(f)
+    c = db.cursor()
+    query = ("SELECT * FROM users WHERE username=?")
+    sel = c.execute(query, (user,))
+    retVal = False
+    for record in sel:
+        retVal = True
+    db.commit()
+    db.close()
+    return retVal
