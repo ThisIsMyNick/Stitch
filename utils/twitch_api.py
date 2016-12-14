ebc7512d6aa7666713a0799b4008b9b77042530b
Harry Lum <hlum1@stuy.edu>
2016-12-14 12:47:01 -0500

    fix some things

--- utils/twitch_api.py
+++ utils/twitch_api.py
@@ -1,9 +1,12 @@
 import httplib, urllib, base64, json
-
-def key():
+import os
+os.chdir('..')
+def keys():
 	k = open("keys.csv", "r").readline();
-	k = k.split(",")[2]
+	k = k.split(",")
 	return k
+keys = keys()
 
-    
- 
+x = urllib.urlopen('https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=%s&redirect_uri=http://127.0.0.1:5000/&scope=chat_login'%(keys[0]))
+html = x.read()
+print(html)
--- utils/wishlist.py
+++ utils/wishlist.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 
+#add to profile (as wishlist table)
 def getWishlist(username):
     ans = []
     db = sqlite3.connect("data/database.db")
@@ -11,22 +12,34 @@ def getWishlist(username):
     db.close()
     return ans
 
-    
+#add to gamepage(+ button)
 def addWishlist(username,game):
     db = sqlite3.connect("data/database.db")
     c = db.cursor()
     data = c.execute("INSERT INTO wishlist VALUES(?,?)",(username,game,))
     db.commit()
     db.close()
-    
+
+#add to profile (- button)    
 def removeWishlist(username,game):
     db = sqlite3.connect("data/database.db")
     c = db.cursor()
-    data = c.execute("DELETE FROM wishlist WHERE username=username AND games=game",(username,game,))
+    data = c.execute("DELETE FROM wishlist WHERE username=? AND games=?",(username,game,))
     db.commit()
     db.close()
 
+#add to gamepage (as table of users w/ that game on wishlist)
+def getUsersFor(game):
+    ans = []
+    db = sqlite3.connect("data/database.db")
+    c = db.cursor()
+    data = c.execute("SELECT username FROM wishlist WHERE games=?",(game,))  
+    for x in data:
+        ans.append(x)
+    db.close()
+    return ans
     
+
 
 '''
 os.chdir("..")
